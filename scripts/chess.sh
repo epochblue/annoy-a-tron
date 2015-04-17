7445c3ee3ca4b736fcfe408b89e85671a5ada8c7
Adam Stracener <AdamStracener@GMail.com>
2015-04-17 16:45:35 -0500

    How about a nice game of chess?

---
+++ scripts/chess.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Starts to read Moby Dick.. the whole book
+
+open /Applications/Chess.app && wget -q http://www.moviesounds.com/wargames/chess2.wav && afplay chess2.wav && rm chess2.wav
