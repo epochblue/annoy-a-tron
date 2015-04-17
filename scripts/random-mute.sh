a14e6d95253944cb95eff53b18c9518bec940027
Bill Israel <bill.israel@gmail.com>
2015-04-17 13:59:27 -0500

    Mute for a random period of time

--- scripts/random-mute.sh
+++ scripts/random-mute.sh
@@ -1,5 +1,8 @@
-#/bin/env sh
+#!/usr/bin/env sh
+
 VOLUME=$(osascript -e "output volume of (get volume settings)")
+DELAY=$(expr $RANDOM % 4)
+
 osascript -e 'set volume 0'
-sleep 2
+sleep $DELAY
 osascript -e "set volume output volume $VOLUME"
