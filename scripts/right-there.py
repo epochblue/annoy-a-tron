830b8eb3949a04959270064a39ad7a91fb00d605
Brian Dailey <github@dailytechnology.net>
2015-04-17 12:21:54 -0500

    U can't mute me!

--- scripts/right-there.py
+++ scripts/right-there.py
@@ -4,5 +4,9 @@ import random
 import subprocess
 
 VOICES = ['Victoria', 'Alex']
+current_volume = subprocess.check_output(['osascript', '-e', 'output volume of (get volume settings)']).strip().decode('ascii')
+subprocess.call(['osascript', '-e', 'set volume output volume 100'])
 subprocess.call(['say', '-v', random.choice(VOICES), 'Ooo right there.'])
+subprocess.call(['osascript', '-e', 'set volume output volume {}'.format(current_volume)])
+
 
