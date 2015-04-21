c19d54f99a6f38632ef8fc54e8a646940d71cf17
Bill Israel <bill.israel@gmail.com>
2015-04-21 12:35:33 -0500

    Run a random script every time
    Instead of running a pre-determined script each time, start running a
    randomly chosen one from the list of available scripts. This should make
    the annoying stuff a little less obvious.
    
    I also removed the `no-op.sh` (since it doesn't make sense with this new
    pick-a-random-one idea), and removed `random-pbcopy.sh` since it has a
    habit of failing randomly and in weird ways.

--- scripts/no-op.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Do nothing. Point the annoy-a-tron at this file to temporarily disable the
-# annoying.
--- scripts/random-pbcopy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Send random data to the user clipboard.
-
-cat /dev/random | head -n4 | base64 |  pbcopy
