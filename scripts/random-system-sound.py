f4689ea4b8255d52aa557ac74fead33386fbfa36
Bill Israel <bill.israel@gmail.com>
2015-05-10 16:41:27 -0500

    Repeat system sounds up to 3 times

--- scripts/random-system-sound.py
+++ scripts/random-system-sound.py
@@ -6,7 +6,9 @@ import subprocess
 
 SOUNDS_DIR = '/System/Library/Sounds'
 SOUNDS = os.listdir(SOUNDS_DIR)
+REPEAT = random.choice(range(1, 4))
 
 to_play = os.path.join(SOUNDS_DIR, random.choice(SOUNDS))
-subprocess.call(['afplay', '-v', str(random.choice(range(10, 51))), to_play])
+for x in range(REPEAT):
+    subprocess.call(['afplay', '-v', str(random.choice(range(10, 51))), to_play])
 
